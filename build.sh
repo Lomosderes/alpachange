20b4646f4ad4172f2add8adefb10c4f2281e9e0c
Lomosderes <176005885+Lomosderes@users.noreply.github.com>
2026-04-28 02:42:51 -0500

    v 1.2.2.2

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 # build.sh
+set -e # Detener el script si algún comando falla
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+# Usamos --clear para limpiar cualquier basura previa
+python manage.py collectstatic --noinput --clear
 
 echo "Running migrations..."
 python manage.py migrate --noinput
